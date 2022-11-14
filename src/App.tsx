62120f352c1abd795baf781d8f908d2f54f7cb40
I566470 <zhuxian.luan@sap.com>
2022-11-14 14:19:36 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App mt-10px ma4 m-1">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
